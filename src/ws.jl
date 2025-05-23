276916e57ec28321918f4bd5b3e126a0345d11ed
vicadlsn <cadinspect@gmail.com>
2025-05-23 04:59:13 +0300

    обночила функцию передачи сообщения клиенту (лучшее решение/особь в популяции)

--- src/ws.jl
+++ src/ws.jl
@@ -1,7 +1,7 @@
 using HTTP
 using JSON
 
-function send_optimization_data(ws, method_id, client_id, request_id, iteration, best_fitness, best_solution, population)
+function send_optimization_data(ws, method_id, client_id, request_id, iteration, best_fitness, best_solution, current_best_fitness, current_best_solution, population)
     data = Dict(
         "action" => "iteration",
         "client_id" => client_id,
@@ -10,11 +10,13 @@ function send_optimization_data(ws, method_id, client_id, request_id, iteration,
         "iteration" => iteration,
         "population" => population,
         "best_fitness" => best_fitness,
-        "best_solution" => best_solution
+        "best_solution" => best_solution,
+        "current_best_fitness" => current_best_fitness,
+        "current_best_solution" => current_best_solution
     )
     try
         HTTP.send(ws, JSON.json(data))
-        @debug "Sent iteration data" method_id=method_id iteration=iteration
+        @debug "Sent data" method_id=method_id iteration=iteration
     catch e
         @error "Error sending iteration data: $e" method_id=method_id
     end
